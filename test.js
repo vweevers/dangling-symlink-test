715296aaa486ee682b6af1dee447b93b90f216c4
Vincent Weevers <mail@vincentweevers.nl>
2014-12-03 11:46:35 +0100

    ..

---
+++ test.js
@@ -0,0 +1,31 @@
+var test   = require('tape')
+  , path   = require('path')
+  , fs     = require('fs')
+  , tmpDir = path.join(__dirname, 'tmp')
+  , mkdirp = require('mkdirp')
+  , rimraf = require('rimraf')
+  , after  = require('after')
+
+test('relative symlink', function(t){
+  prepare(function (err){
+    var link = path.join(tmpDir, 'a/link')
+
+    // relative to "a" (link's parent dir)
+    var target = '../actual' 
+
+    console.log('symlink("%s", "%s")', target, link)
+
+    fs.symlink(target, link, 'junction', function(err){
+      if (err) console.log(err)
+      
+      t.ok(fs.existsSync(path.join(link, 'b')))
+      rimraf(tmpDir, t.end)
+    })
+  })
+
+  function prepare(done) {
+    var next = after(2, done)
+    mkdirp(path.join(tmpDir, 'a'), next)
+    mkdirp(path.join(tmpDir, 'actual/b'), next)
+  }
+})
